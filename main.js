2e28d49d9159129a5866c2cf4906168d353c0488
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-08 16:19:52 +0100

    Creo l'attivazione del menù al click

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
 const button = document.querySelector('.fa-bars');
 console.log(button);
 
+// SELEZIONO LA CLASSE HAMBURGER-MENU
+
+const burger = document.querySelector('.hamburger-menu');
+console.log(burger);
+
+// CREO L'ATTIVAZIONE DEL MENÙ AL CLICK
+
+button.addEventListener('click', function() {
+    console.log('di nulla, figurati')
+    
+})
