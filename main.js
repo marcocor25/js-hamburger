16adf42db7b27c6e50860726fa07b9ef9af30db9
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-08 16:27:11 +0100

    Esercizio completato!

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-// SELEZIONO L'ICONA DEL BURGER MENU
+// SELEZIONO L'ICONA DELL'HAMBURGER-MENÙ
 
 const button = document.querySelector('.fa-bars');
 console.log(button);
 
-// SELEZIONO LA CLASSE HAMBURGER-MENU
+// SELEZIONO LA CLASSE HAMBURGER-MENÙ
 
 const burger = document.querySelector('.hamburger-menu');
 console.log(burger);
 
-// CREO L'ATTIVAZIONE DEL MENÙ AL CLICK
+// SELEZIONO L'ICONA PER CHIUDERE IL MENÙ
+
+const buttonClose = document.querySelector('.close');
+console.log(buttonClose);
+
+// CREO L'APERTURA DEL MENÙ AL CLICK
 
 button.addEventListener('click', function() {
-    console.log('di nulla, figurati')
-    
+    burger.style.display = 'block';
 })
+
+// CREO LA CHIUSURA DEL MENÙ AL CLICK
+
+buttonClose.addEventListener('click', function() {
+    console.log('Finalmente ho finito un esercizio JavaScript senza sclerare.')
+    burger.style.display = 'none';
+})
+
+// FINE
